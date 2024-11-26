65af8a10c72b4871226a538965067ab8ac3f731d
richardR144 <richard.rigollet@lapiscine.pro>
2024-11-26 10:41:31 +0100

    articleShowController et articleShow.html.twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -45,10 +47,67 @@ class ArticleController extends AbstractController
 
         ];
 
-            return $this->render('articles_list.html.twig', [
-                'articles' => $articles
+        return $this->render('articles_list.html.twig', [
+            'articles' => $articles
         ]);
 
     }
 
+    #[Route('/showArticle/{id}', 'showArticle', methods: ['GET'])]
+    public function showArticle()
+    {
+        $request = Request::createFromGlobals();
+        $id = $request->query->get('id');
+
+
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Content of article 1',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Content of article 2',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Content of article 3',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 4,
+                'title' => 'Article 4',
+                'content' => 'Content of article 4',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 5,
+                'title' => 'Article 5',
+                'content' => 'Content of article 5',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ]
+
+        ];
+        //Je créai un variable qui doit contenir l'article trouvé pour l'intancié à null
+        $articleFound = null;
+        //pour chaque article de la liste trové, je check si son id correspond à l'id récupérer de l'url
+        // et si c'est bon je le stocke dans ma variable $articleFound
+        foreach ($articles as $article) {
+            if ($article['id'] === (int) $id) {
+                $articleFound = $article;
+            }
+        }
+        //je créai une réponse HTTP contenant le HTML de mon fichier twig
+        //pour ce faire, j'utilise la méthode render de l'abstract controller qui prend en param le twig
+        //et en second le tableau contenant les variables utilisables dans le twig
+        return $this->render('article_show.html.twig', [
+            'article' => $articleFound
+        ]);
+    }
 }
+
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
